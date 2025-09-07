4436ce9fd53af510e05a14a384e20297355eff3c
ameyeree <161140957+ameyeree@users.noreply.github.com>
2025-09-07 12:34:23 -0700

    Initial commit of finished pbl_2 design code and testbench

--- Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sim_1/new/tb_and_mux_combinational.v
+++ Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sim_1/new/tb_and_mux_combinational.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: SSU
+// Engineer: Anthony Meyer Slechta
 // 
-// Create Date: 09/07/2025 12:00:34 PM
-// Design Name: 
+// Create Date: 09/07/2025 12:02:58 PM
+// Design Name: And Mux Combinational
 // Module Name: tb_and_mux_combinational
-// Project Name: 
-// Target Devices: 
+// Project Name: PBL 2: AND-MUX Combinational
+// Target Devices: PYNQ-Z2
 // Tool Versions: 
-// Description: 
+// Description: Testing and-mux combinational logic and testbench simulation.
 // 
 // Dependencies: 
 // 
@@ -20,7 +20,78 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb_and_mux_combinational(
+module tb_and_mux_combinational;
+    reg a;
+    reg b;
+    reg c;
+    reg d;
+    reg sel;
+    wire e;
 
+    // Instantiate DUT
+    and_mux_combinational uut (
+        .a(a),
+        .b(b),
+        .c(c),
+        .d(d),
+        .sel(sel),
+        .e(e)
     );
+
+    // Test the 4 test cases given for assignment
+    initial begin
+        // initialization
+        sel = 1'b0;
+        a = 1'b0;
+        b = 1'b0;
+        c = 1'b0;
+        d = 1'b0;
+
+        // sel = 0, a=b=d=1, c = 0
+        // Expected output: e = 1
+        #2;
+        sel = 1'b0;
+        a = 1'b1;
+        b = 1'b1;
+        c = 1'b0;
+        d = 1'b1;
+
+        // sel = 1, a=b=d=1, c = 0
+        // Expected output: e = 0
+        #10;
+        sel = 1'b1;
+        a = 1'b1;
+        b = 1'b1;
+        c = 1'b0;
+        d = 1'b1;
+
+        // sel = 0, a = 0, b=c=d=1
+        // Expected output: e = 0
+        #10;
+        sel = 1'b0;
+        a = 1'b0;
+        b = 1'b1;
+        c = 1'b1;
+        d = 1'b1;
+
+        // sel = 1, a = 0, b=c=d=1
+        // Expected output: e = 1
+        #10;
+        sel = 1'b1;
+        a = 1'b0;
+        b = 1'b1;
+        c = 1'b1;
+        d = 1'b1;
+        
+        #10;
+
+        $finish;
+    end
+
+    // Output results
+    initial begin
+        $display("sel\ta\tb\tc\td\t\te");
+        $monitor("%b\t%b\t%b\t%b\t%b\t\t%b", sel, a, b, c, d, e);
+    end    
+
 endmodule
--- Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sources_1/new/and_mux_combinational.v
+++ Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sources_1/new/and_mux_combinational.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: SSU
+// Engineer: Anthony Meyer Slechta
 // 
 // Create Date: 09/07/2025 11:59:46 AM
-// Design Name: 
+// Design Name: And Mux Combinational
 // Module Name: and_mux_combinational
-// Project Name: 
-// Target Devices: 
+// Project Name: PBL 2: AND-MUX Combinational
+// Target Devices: PYNQ-Z2
 // Tool Versions: 
-// Description: 
+// Description: Testing and-mux combinational logic and testbench simulation.
 // 
 // Dependencies: 
 // 
@@ -21,6 +21,15 @@
 
 
 module and_mux_combinational(
-
+    input a,
+    input b,
+    input c,
+    input d,
+    input sel,
+    output e
     );
+
+    // two and gates: a & b and c & d
+    // If sel == 0, e = a & b, else e = c & d
+    assign e = ~sel ? (a & b) : (c & d);
 endmodule
