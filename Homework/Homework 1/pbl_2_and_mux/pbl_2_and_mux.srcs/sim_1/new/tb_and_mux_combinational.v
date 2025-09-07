60f053d844b7350960662dd321083f29a7a06568
ameyeree <161140957+ameyeree@users.noreply.github.com>
2025-09-07 12:01:33 -0700

    Initial Creation of pbl_2 project folders

---
+++ Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sim_1/new/tb_and_mux_combinational.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2025 12:00:34 PM
+// Design Name: 
+// Module Name: tb_and_mux_combinational
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_and_mux_combinational(
+
+    );
+endmodule
---
+++ Homework/Homework 1/pbl_2_and_mux/pbl_2_and_mux.srcs/sources_1/new/and_mux_combinational.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2025 11:59:46 AM
+// Design Name: 
+// Module Name: and_mux_combinational
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_mux_combinational(
+
+    );
+endmodule
